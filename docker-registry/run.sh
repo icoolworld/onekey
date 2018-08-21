76ecd2843480ee5a3d920cd07d5fbe0593a4c1c3
root <root@registry.17173.com>
2018-08-21 05:16:37 -0400

    ok

--- docker-registry/run.sh
+++ docker-registry/run.sh
@@ -9,7 +9,7 @@ cd /data/webserver/docker-registry/
 host='registry.17173.com'
 
 mkdir -p conf/certs && cd conf/
-openssl req -newkey rsa:4096 -nodes -sha256 -days 3650 -keyout certs/registry.key -x509 -out certs/registry.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=website Inc./OU=Web Security/CN=$host"
+openssl req -newkey rsa:4096 -nodes -sha256 -days 3650 -keyout certs/$host.key -x509 -out certs/$host.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=website Inc./OU=Web Security/CN=$host"
 cd ../
 
 # http support
